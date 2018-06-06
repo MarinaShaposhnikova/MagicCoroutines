f5db2fc7b385c5df5d596a66053ed0cdd85ed429
marynashaposhnikova <maryna.shaposhnikova@mobilestrategy.nl>
2018-06-06 18:01:09 +0300

    Add measuring for current coroutine

--- app/src/main/java/com/meier/marina/magiccoroutines/MainViewModel.kt
+++ app/src/main/java/com/meier/marina/magiccoroutines/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.meier.marina.magiccoroutines
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.meier.marina.magiccoroutines.data.MainRepository
 import com.meier.marina.magiccoroutines.data.User
+import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 
 class MainViewModel : ViewModel() {
@@ -13,10 +15,12 @@ class MainViewModel : ViewModel() {
     val userLiveData = MutableLiveData<User>()
 
     init {
+        val startTime = System.currentTimeMillis()
         launch {
             val user = mainRepository.getUser("id")
             val userWithWand = mainRepository.getWand(user)
             userLiveData.postValue(userWithWand)
+            Log.d(LOG_TAG, "${System.currentTimeMillis() - startTime}")
         }
     }
 }
---
+++ app/src/main/java/com/meier/marina/magiccoroutines/Utils.kt
@@ -0,0 +1,3 @@
+package com.meier.marina.magiccoroutines
+
+const val LOG_TAG = "marina_meier"
